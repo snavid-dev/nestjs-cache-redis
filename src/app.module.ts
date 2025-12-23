f6ae64251583a4690aa965579728b0479528e9b7
SNA Rahanet <navidjan.azimi132@gmail.com>
2025-12-23 11:30:41 +0430

    the cache management using redis is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
+import { CacheTestController } from './cache-test.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      stores: [new KeyvRedis('redis://localhost:6379')],
+      ttl: 300_000,
+    }),
+  ],
+  controllers: [AppController, CacheTestController],
   providers: [AppService],
 })
 export class AppModule {}
